3194a05fc66e5b8f485055536e731bffafac1970
CIPHERTron <pritish.samal918@gmail.com>
2024-07-26 20:32:02 +0530

    fix(temp): remove user model from schema

---
+++ apps/api/prisma/migrations/20240726135413_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
+);

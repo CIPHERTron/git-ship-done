dae023ac8ea31066d94940e9c99d125f2500d2c2
CIPHERTron <pritish.samal918@gmail.com>
2024-07-27 18:39:14 +0530

    feat: add basic nav bar

--- apps/client/eslint.config.mjs
+++ apps/client/eslint.config.mjs
@@ -10,55 +10,62 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [...compat.extends(
+export default [
+  ...compat.extends(
     "next/core-web-vitals",
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
-    "prettier",
-), {
+    "prettier"
+  ),
+  {
     plugins: {
-        "@typescript-eslint": typescriptEslint,
-        react,
-        prettier,
+      "@typescript-eslint": typescriptEslint,
+      react,
+      prettier,
     },
 
     languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 2020,
-        sourceType: "module",
+      parser: tsParser,
+      ecmaVersion: 2020,
+      sourceType: "module",
 
-        parserOptions: {
-            ecmaFeatures: {
-                jsx: true,
-            },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
         },
+      },
     },
 
     settings: {
-        react: {
-            version: "detect",
-        },
+      react: {
+        version: "detect",
+      },
     },
 
     rules: {
-        "react/react-in-jsx-scope": "off",
+      "react/react-in-jsx-scope": "off",
 
-        "@typescript-eslint/no-unused-vars": ["error", {
-            argsIgnorePattern: "^_",
-        }],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
 
-        "prettier/prettier": "error",
+      "prettier/prettier": "error",
     },
-}, {
+  },
+  {
     files: ["**/*.ts", "**/*.tsx"],
 
     rules: {
-        "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
     },
-}];
+  },
+];

b3dab19df1deb86b5e450495b5c0fc8e3a2eae8f
CIPHERTron <pritish.samal918@gmail.com>
2024-07-27 18:32:24 +0530

    feat: configure eslint and prettier

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "~/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "~/components/theme-provider";
+import Navigation from "~/components/shared/Navigation";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <Navigation />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
-
-export default MyApp

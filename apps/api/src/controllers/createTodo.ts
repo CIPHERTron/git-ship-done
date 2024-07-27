543e24f3c92ea374e5c3100b3ded47ccc053917e
CIPHERTron <pritish.samal918@gmail.com>
2024-07-27 11:34:45 +0530

    feat: add update and delete todo db transaction logic

--- apps/api/src/controllers/createTodo.ts
+++ apps/api/src/controllers/createTodo.ts
@@ -2,15 +2,16 @@ import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 import { PrismaClient } from '@prisma/client';
 import { JetStreamClient, StringCodec } from 'nats';
 
-interface CreateTodoRequest {
+interface TodoRequest {
     Body: {
+        id?: number;
         title: string;
         description: string;
     }
 }
 
 export const createTodoController = (prisma: PrismaClient, jetStreamClient: JetStreamClient) => {
-    return async (req: FastifyRequest<CreateTodoRequest>, reply: FastifyReply) => {
+    return async (req: FastifyRequest<TodoRequest>, reply: FastifyReply) => {
         const { title, description } = req.body;
 
         try {
@@ -23,7 +24,7 @@ export const createTodoController = (prisma: PrismaClient, jetStreamClient: JetS
     
             // Publish to-do creation event to jetstream
             const sc = StringCodec();
-            await jetStreamClient.publish('todo.create', sc.encode(JSON.stringify(todo)));
+            await jetStreamClient.publish('replicache.create', sc.encode(JSON.stringify(todo)));
     
             reply.send(todo)
         } catch (error) {
@@ -32,3 +33,52 @@ export const createTodoController = (prisma: PrismaClient, jetStreamClient: JetS
         }
     };
 }
+
+export const updateTodoController = (prisma: PrismaClient, jetStreamClient: JetStreamClient) => {
+    return async (req: FastifyRequest<TodoRequest>, reply: FastifyReply) => {
+      const { id, title, description } = req.body;
+  
+      if (!id) {
+        reply.status(400).send({ error: 'ID is required' });
+        return;
+      }
+  
+      try {
+        const todo = await prisma.todo.update({
+          where: { id },
+          data: {
+            title,
+            description,
+          },
+        });
+  
+        const sc = StringCodec();
+        await jetStreamClient.publish('replicache.update', sc.encode(JSON.stringify(todo)));
+  
+        reply.send(todo);
+      } catch (error) {
+        console.log('Error updating todo:', error);
+        reply.status(500).send({ error: 'Internal Server Error while updating todo' });
+      }
+    };
+};
+
+export const deleteTodoController = (prisma: PrismaClient, jetStreamClient: JetStreamClient) => {
+    return async (req: FastifyRequest<TodoRequest>, reply: FastifyReply) => {
+      const { id } = req.body;
+  
+      try {
+        const todo = await prisma.todo.delete({
+          where: { id },
+        });
+  
+        const sc = StringCodec();
+        await jetStreamClient.publish('replicache.delete', sc.encode(JSON.stringify(todo)));
+  
+        reply.send(todo);
+      } catch (error) {
+        console.log('Error deleting todo:', error);
+        reply.status(500).send({ error: 'Internal Server Error while deleting todo' });
+      }
+    };
+  };

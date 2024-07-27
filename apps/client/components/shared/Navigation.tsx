2e11e17d0a7e2859e20f1deaed3808bb1314a17d
CIPHERTron <pritish.samal918@gmail.com>
2024-07-27 18:39:14 +0530

    feat: add basic nav bar

--- apps/client/components/shared/Navigation.tsx
+++ apps/client/components/shared/Navigation.tsx
@@ -9,27 +9,18 @@ import { ThemeToggle } from "./ThemeToggle";
 
 export default function Navigation() {
   return (
-    <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
-      <div className="flex flex-row justify-between items-center">
-        <div className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
-          <Link
-            href="#"
-            className="flex items-center gap-2 text-lg font-semibold md:text-base"
-          >
-            <Package2 className="h-6 w-6" />
-            <span className="sr-only">Acme Inc</span>
-          </Link>
-          <Link
-            href="#"
-            className="text-muted-foreground transition-colors hover:text-foreground"
-          >
-            GIT SHIP DONE
-          </Link>
-        </div>
+    <header className="sticky top-0 flex justify-between h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+      <div className="w-fit text-lg font-medium">
+        <Link
+          href="#"
+          className="text-muted-foreground transition-colors hover:text-foreground"
+        >
+          GIT-SHIP-DONE
+        </Link>
+      </div>
 
-        <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
-          <ThemeToggle />
-        </div>
+      <div>
+        <ThemeToggle />
       </div>
     </header>
   );

97d3c6b9a13e59c24d9cd79417283510d21e38f8
CIPHERTron <pritish.samal918@gmail.com>
2024-07-29 20:59:54 +0530

    fix: handle delete todo

--- apps/api/src/controllers/replicacheController.ts
+++ apps/api/src/controllers/replicacheController.ts
@@ -65,10 +65,13 @@ export const replicachePush = (prisma: PrismaClient, jetStreamClient: JetStreamC
                           await jetStreamClient.publish('replicache.done', sc.encode(JSON.stringify(message)));
                           break;
                       case 'deleteTodo':
-                          const deletedTodo = await tx.todo.delete({
-                              where: { id: mutation.args.id }
+                          // const deletedTodo = await tx.todo.delete({
+                          //     where: { id: mutation.args.id }
+                          // });
+                          const todoToDelete = await tx.todo.findUnique({
+                            where: { id: mutation.args.id }
                           });
-                          message.data = deletedTodo;
+                          message.data = todoToDelete;
                           await jetStreamClient.publish('replicache.delete', sc.encode(JSON.stringify(message)));
                           break;
                       default:
